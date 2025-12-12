5e371973d86c7a791f4e10247aad1fc172ad3033
Farrukh <sydykovfarruh07@gmail.com>
2025-12-12 11:24:39 +0600

    03: authentication setyp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,84 @@
+"use client"
+
+import { useState } from "react"
+import { Input } from "@/components/ui/input"
+import { authClient } from "@/lib/auth-client";
 import { Button } from "@/components/ui/button"
 
+
 export default function Home() {
+  const { data: session, isPending } = authClient.useSession()
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      name,
+      password,
+    }, {
+      onError: () => {
+        window.alert("Something went wrong while submitting")
+      },
+      onSuccess: () => {
+        window.alert("Success!")
+      }
+    })
+  }
+
+  const onLogin = () => {
+    authClient.signIn.email({
+      email,
+      password,
+    }, {
+      onError: () => {
+        window.alert("Something went wrong while logging in")
+      },
+      onSuccess: () => {
+        window.alert("Success!")
+      }
+    })
+  }
+
+  if (isPending) {
+    return (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-opacity-50 backdrop-blur-sm">
+        <div className="flex flex-col items-center gap-4 p-8 bg-white rounded-lg shadow-2xl">
+          <div className="relative w-16 h-16">
+            <div className="absolute inset-0 border-4 border-gray-200 rounded-full"></div>
+            <div className="absolute inset-0 border-4 border-blue-500 rounded-full border-t-transparent animate-spin"></div>
+          </div>
+          <p className="text-lg font-medium text-gray-700">Loading...</p>
+        </div>
+      </div>
+    ); 
+  }
+
+  if (session) {
+    return(
+      <div className="flex flex-col p4 gap-y-4">
+        <p>Logged in as {session.user.name}</p>
+        <Button onClick={() => authClient.signOut() }>
+          Sign Out
+        </Button>
+      </div>
+    )
+  }
   return (
-    <Button>ShadCN Button</Button>
+    <div className="flex flex-col gap-y-4">
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+        <Input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <Button onClick={onSubmit}>Sign Up</Button>
+      </div>
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <Button onClick={onLogin}>Login</Button>
+      </div>
+    </div>
   )
 }

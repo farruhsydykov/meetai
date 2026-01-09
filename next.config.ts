234a7b4b1b20376256168e48fe52651b35f1aefb
Farrukh <sydykovfarruh07@gmail.com>
2026-01-09 20:24:44 +0600

    24: background jobs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['https://rosy-verena-unelaborately.ngrok-free.dev', 'localhost:3000']
 };
 
 export default nextConfig;
